353bfdc0ae1317fa3a74ba1ddd08344a356c8e0d
Wael el Hachimi <wael@unik.ma>
2013-09-22 10:02:17 +0100

    Add full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
+	let(:base_title) {"Twitter tweet tweet"}
   describe "Home page" do
   	it "should have the content 'Twitter' " do
   		visit '/static_pages/home'
   		expect(page).to have_content('Twitter')
-  		expect(page).to have_title("Twitter tweet tweet | Home")
+  		expect(page).to have_title("#{base_title} | Home")
   	end
   end
 
@@ -14,7 +14,7 @@ describe "StaticPages" do
   	it "should have the content 'Help' " do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
-  		expect(page).to have_title("Twitter tweet tweet | Help")
+  		expect(page).to have_title("#{base_title} | Help")
   	end
   end
 
@@ -22,7 +22,7 @@ describe "StaticPages" do
   	it "should have the content 'About' " do
   		visit '/static_pages/about'
   		expect(page).to have_content('About')
-  		expect(page).to have_title("Twitter tweet tweet | About")
+  		expect(page).to have_title("#{base_title} | About")
   	end
   end
 end
